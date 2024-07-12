6659d84aaceb4b01c71ff8eeb4d722c4aa0beaed
Jake Morrison <jake@cogini.com>
2024-07-12 15:14:24 -0500

    Support dynamic configuration in params

--- lib/tesla/middleware/basic_auth.ex
+++ lib/tesla/middleware/basic_auth.ex
@@ -13,19 +13,45 @@ defmodule Tesla.Middleware.BasicAuth do
     # static configuration
     plug Tesla.Middleware.BasicAuth, username: "user", password: "pass"
 
+    # dynamic configuration
+    plug Tesla.Middleware.BasicAuth, username: {:foo, :user}, {:foo, :pass}
+    plug Tesla.Middleware.BasicAuth, username: {:foo, :user, "user"}, {:foo, :pass, "pass"}
+    plug Tesla.Middleware.BasicAuth, username: "user", &get_pass/1
+    plug Tesla.Middleware.BasicAuth, &get_auth/1
+
     # dynamic user & pass
     def new(username, password, opts \\\\ %{}) do
       Tesla.client [
         {Tesla.Middleware.BasicAuth, Map.merge(%{username: username, password: password}, opts)}
       ]
     end
+
+    defp get_pass(_key) do
+      Application.get_env(:foo, :pass)
+    end
   end
   ```
 
   ## Options
 
-  - `:username` - username (defaults to `""`)
-  - `:password` - password (defaults to `""`)
+  The input takes a map or keyword list with the following keys:
+
+  Simple string configuration:
+  - `:username` - string (defaults to `""`)
+  - `:password` - string (defaults to `""`)
+
+  Read from Application environment:
+  - `:username` - {:the_app, :config_key}
+  - `:username` - {:the_app, :config_key, "default"}
+
+  - `:password` - {:the_app, :config_key}
+  - `:password` - {:the_app, :config_key, "default"}
+
+  Call a function:
+  - `:username` - Function with arity 1
+  - `:password` - Function with arity 1
+
+  You can also specify a function which returns a map.
   """
 
   @behaviour Tesla.Middleware
@@ -46,11 +72,31 @@ defmodule Tesla.Middleware.BasicAuth do
     |> create_header()
   end
 
-  defp authorization_vars(opts) do
-    %{
-      username: opts[:username] || "",
-      password: opts[:password] || ""
-    }
+  @spec authorization_vars(Keyword.t() | map() | function()) :: map()
+  def authorization_vars(fun) when is_function(fun) do
+    fun.()
+  end
+
+  def authorization_vars(opts) when is_map(opts) or is_list(opts) do
+    for key <- [:username, :password], into: %{} do
+      authorization_var(key, opts[key] || "")
+    end
+  end
+
+  defp authorization_var(key, {app, config_key}) do
+    {key, Application.get_env(app, config_key, "")}
+  end
+
+  defp authorization_var(key, {app, config_key, default}) do
+    {key, Application.get_env(app, config_key, default)}
+  end
+
+  defp authorization_var(key, val) when is_function(val) do
+    {key, val.(key)}
+  end
+
+  defp authorization_var(key, val) do
+    {key, val}
   end
 
   defp create_header(auth) do
